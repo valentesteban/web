61abca3685930f7ebc3abc9d92c5a3d260dfba62
Clockech <valente.estebann@gmail.com>
2023-10-26 01:17:01 -0300

    feat: create the entire page lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <MaxWidthWrapper>
+        <div className="flex flex-col mt-[80px] pt-5 pr-1 pb-5 pl-[30px] text-[0.875rem] leading-[1.05625rem] justify-center">
+          <h1>Esteban Valente</h1>
+          <p>
+            Updated <time dateTime="2023-10-26" /> October 26, 2023
+          </p>
+
+          <h2 className="py-5">About me:</h2>
+
+          <div className="px-2">
+            <ul>
+              <li>• Born in Rosario, Argentina</li>
+              <li>• Based in Montevideo, Uruguay</li>
+              <li>• Designer and programmer</li>
+              <li>• Interested in crafting delightful interfaces</li>
+              <li>
+                • Founder of{" "}
+                <Link
+                  href={"https://clockech.com"}
+                  className="hover:bg-gray-100 underline"
+                  target="_blank"
+                >
+                  Clockech
+                </Link>
+                , a software company
+                <ul className="px-5">
+                  ◦ Now: Making real-time analytics friendly at{" "}
+                  <Link
+                    href="https://clockech.com"
+                    className="hover:bg-gray-100 underline"
+                    target="_blank"
+                  >
+                    Clockech
+                  </Link>
+                </ul>
+                <ul className="px-5">
+                  ◦ Past: Created{" "}
+                  <Link
+                    href="https://checktoken.com"
+                    className="hover:bg-gray-100 underline"
+                    target="_blank"
+                  >
+                    CheckToken
+                  </Link>
+                  , An analytics OpenAI report app
+                </ul>
+              </li>
+              <li>
+                • Founder of{" "}
+                <Link
+                  href={"https://acentus.agency"}
+                  className="hover:bg-gray-100 underline"
+                  target="_blank"
+                >
+                  Acentus
+                </Link>
+                , a design agency
+                <ul className="px-5">
+                  ◦ We make different things when we can, like{" "}
+                  <Link
+                    href={"https://grupoboing.com"}
+                    className="underline"
+                    target="_blank"
+                  ></Link>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <h2 className="py-5">Find me:</h2>
+
+          <div className="px-2">
+            <ul>
+              <li>
+                •{" "}
+                <Link
+                  href={"https://twitter.com/joesvart"}
+                  className="underline hover:bg-gray-100"
+                  target="_blank"
+                >
+                  Twitter
+                </Link>
+              </li>
+              <li>
+                •{" "}
+                <Link
+                  href={"https://linkedin.com/in/valentesteban/"}
+                  className="underline hover:bg-gray-100"
+                  target="_blank"
+                >
+                  Linkedin
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <h2 className="py-5">Contact me:</h2>
+
+          <div className="px-2">
+            <ul>
+              <li>
+                •{" "}
+                <Link
+                  href={"mailto:esteban@clockech.com"}
+                  className="underline hover:bg-gray-100"
+                  target="_blank"
+                >
+                  Email
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </MaxWidthWrapper>
+    </>
+  );
 }
